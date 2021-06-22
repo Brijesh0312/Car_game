eb920774bdcdbc570f011e26b34915b00b0dc168
Brijesh <brijeshyerram@gmail.com>
2021-06-22 15:02:58 +0530

    first commit

---
+++ Assets/Scripts/charactercontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class charactercontroller : MonoBehaviour
+{
+    CharacterController Mover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mover = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 coordinates = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Mover.Move(coordinates);
+       transform.Rotate(0,0,Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Assets/Scripts/iskinematicactivivator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iskinematicactivivator : MonoBehaviour
+{
+    Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad >= 1)
+        {
+            rigid.isKinematic = true;
+        }
+    }
+}
